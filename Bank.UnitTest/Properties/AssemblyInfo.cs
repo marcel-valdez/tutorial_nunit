14021ba05213f32e01bb28a02b797a79cfdf515b
marcel-valdez <marcel.valdez@live.com>
2012-08-17 17:24:07 -0500

    Added project files

---
+++ Bank.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssemblyInfo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyInfo")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("26c7fd68-e038-42f3-833f-dc6287130bed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Bank/Account.cs
@@ -0,0 +1,32 @@
+﻿namespace Bank
+{
+    using System;
+
+    public class Account
+    {
+        private decimal balance;
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            balance -= amount;
+        }
+
+        public void TransferFunds(Account destination, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+        }
+    }
+}
